cce68c79448e35153d1f8e11b1ce89eab60da8ac
Omar <omarraad@gmail.com>
2022-05-23 11:15:57 +0300

    initial commit

---
+++ src/main/java/com/gtss/mnp_manager/MnpManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gtss.mnp_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnpManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnpManagerApplication.class, args);
+	}
+
+}
